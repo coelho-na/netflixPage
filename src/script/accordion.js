76c7c27cb4f6e10125bf2208571921516e909c39
Nadine Coelho <nadinecoelho9@gmail.com>
2021-05-22 17:47:59 -0300

    finish accordion script

---
+++ src/script/accordion.js
@@ -0,0 +1,13 @@
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach((accordionHeader) => {
+  accordionHeader.addEventListener("click", () => {
+    accordionHeader.classList.toggle("active");
+    const accordionBody = accordionHeader.nextElementSibling;
+    if (accordionHeader.classList.contains("active")) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
+    } else {
+      accordionBody.style.maxHeight = 0;
+    }
+  });
+});
